41c5314b40920e9fcbbbc0ae3ee207fb59adb98e
Kevin Chen <xkevinchen@gmail.com>
2019-01-04 15:14:54 -0500

    Correct frontend errors

--- frontend/reducers/errors/session_errors_reducer.js
+++ frontend/reducers/errors/session_errors_reducer.js
@@ -4,7 +4,7 @@ import {
   CLEAR_SESSION_ERRORS
 } from '../../actions/session_actions';
 
-const sessionErrorsReducer = ( state = {}, action ) => {
+const sessionErrorsReducer = ( state = [], action ) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
