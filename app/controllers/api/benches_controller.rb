c9e364d45b754015829cbd01de4b6fa21a2e0b52
Kevin Chen <xkevinchen@gmail.com>
2019-01-09 13:47:49 -0500

    Add benches model, view, and controller

---
+++ app/controllers/api/benches_controller.rb
@@ -0,0 +1,20 @@
+class Api::BenchesController < ApplicationController
+  def index
+    @benches = Bench.all
+  end
+
+  def create
+    @bench = Bench.create!(bench_params)
+    render :show
+  end
+
+  def show
+    @bench = Bench.find(params[:id])
+  end
+
+  private
+
+  def bench_params
+    params.require(:bench).permit(:description, :lat, :lng)
+  end
+end
